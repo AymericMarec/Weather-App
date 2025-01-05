c330ab51b4b394498efcfb888255e2ffa7e50c24
dnoemie <noemie.dublanc@ynov.com>
2025-01-05 17:18:16 +0100

    push background color function for cahnge the color + start settings

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Models;
 using System;
 using System.Collections.Generic;
+using Avalonia.Media;
 
 namespace GetStartedApp.ViewModels;
 
@@ -17,7 +18,6 @@ public partial class HomeViewModel : ViewModelBase
     public string Lon { get; }
     public string Sunrise { get; }
     public string Sunset { get; }
-
     public HomeViewModel()
     {
         List<string> FavCity = Settings.getFavCity();
@@ -34,7 +34,8 @@ public partial class HomeViewModel : ViewModelBase
         Lon = result.GetLon();
         Sunrise = result.GetSunrise();
         Sunset = result.GetSunset();
-
+        BackgroundColor = UpdateBackgroundColor();
+        Console.WriteLine(BackgroundColor);
 
         
 
@@ -47,5 +48,35 @@ public partial class HomeViewModel : ViewModelBase
         //     }
         // }
     }
+
+
+    private Color _backgroundColor;
+    public Color BackgroundColor 
+    {
+        get => _backgroundColor;
+        set
+        {
+            _backgroundColor = value;
+            OnPropertyChanged(nameof(BackgroundColor));
+        }
+    }
+
+    private Color UpdateBackgroundColor()
+    {
+        DateTime currentTime = DateTime.Now;
+        TimeSpan sunriseTime = TimeSpan.Parse(Sunrise);
+        TimeSpan sunsetTime = TimeSpan.Parse(Sunset);
+
+        if (currentTime.TimeOfDay >= sunriseTime && currentTime.TimeOfDay <= sunsetTime)
+        {
+            return Color.Parse("#FF57B8BF");
+        }
+        else
+        {
+            return Color.Parse("#FF171A52");
+        }
+    }
+
+
     // Console.WriteLine("coucou");
 }
